e6807d674b8a661ac44d093a5567f6c9ac725881
acpDaniel <100088582+acpDaniel@users.noreply.github.com>
2023-10-01 09:52:17 -0300

    Create class.consulta.php
    criacao classe consulta

---
+++ code/class/class.consulta.php
@@ -0,0 +1,47 @@
+<?php
+class ConsultaDentista {
+    private $dentista_executor;
+    private $data;
+    private $horario;
+    private $duracao_consulta;
+    
+    public function __construct($dentista_executor, $data, $horario, $duracao_consulta) {
+        $this->dentista_executor = $dentista_executor;
+        $this->data = $data;
+        $this->horario = $horario;
+        $this->duracao_consulta = $duracao_consulta;
+    }
+    
+    public function getDentistaExecutor() {
+        return $this->dentista_executor;
+    }
+    
+    public function setDentistaExecutor($dentista_executor) {
+        $this->dentista_executor = $dentista_executor;
+    }
+    
+    public function getData() {
+        return $this->data;
+    }
+    
+    public function setData($data) {
+        $this->data = $data;
+    }
+    
+    public function getHorario() {
+        return $this->horario;
+    }
+    
+    public function setHorario($horario) {
+        $this->horario = $horario;
+    }
+    
+    public function getDuracaoConsulta() {
+        return $this->duracao_consulta;
+    }
+    
+    public function setDuracaoConsulta($duracao_consulta) {
+        $this->duracao_consulta = $duracao_consulta;
+    }
+}
+?>
