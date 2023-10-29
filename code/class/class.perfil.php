980978169ac06ea03f57aaf2883f5a4bbdf3c5f4
B-Nic <145769988+B-Nic@users.noreply.github.com>
2023-10-29 20:21:36 -0300

    Create class.perfil.php

---
+++ code/class/class.perfil.php
@@ -0,0 +1,13 @@
+<?php
+
+class Perfil {
+    protected array $funcionalidade_permitidas;
+
+    public function adicionaFuncionalidade(){
+        return 0; 
+    }
+    public function retiraFuncionalidade(){
+        return 0;
+    }  
+}
+?>
