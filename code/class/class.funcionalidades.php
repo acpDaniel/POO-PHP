a2f3a5ff79510a83e2eec423f3b02b68898fe0aa
B-Nic <145769988+B-Nic@users.noreply.github.com>
2023-10-29 21:29:55 -0300

    Create class.funcionalidades.php

---
+++ code/class/class.funcionalidades.php
@@ -0,0 +1,19 @@
+<?php
+class Funcionalidades {
+   private array funcionalidades; #dicionario
+  
+   public function __construct(){
+
+   }
+   
+    public function AdicionaFuncionalidade(){
+      
+    }
+    public function ExecutaFuncionalidade(usuario_logado, objeto, nome_funcionalidade){
+
+    }
+    public function ValidaPermissao(usuario_logado, nome_classe, nome_funcionalidade){
+
+    }
+}
+?>
