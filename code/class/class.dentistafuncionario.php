48d0f07a3312854cf95addc79e3ca1e6921d392b
acpDaniel <100088582+acpDaniel@users.noreply.github.com>
2023-11-07 21:52:12 -0300

    Update class.dentistafuncionario.php

--- code/class/class.dentistafuncionario.php
+++ code/class/class.dentistafuncionario.php
@@ -3,15 +3,15 @@
 	include_once "class.dentista.php";
     
 	class DentistaFuncionario extends Dentista {
-		private float $salario;
+		private float $salario_fixo;
 
 		public function __construct(string $nome, string $email, string $telefone, string $cpf, string $endereco, string $cro, array $especialidade, float $salario) {
 			parent::__construct($nome, $email, $telefone, $cpf, $endereco, $cro, $especialidade, $salario);
-			$this->salario = $salario;
+			$this->salario_fixo = $salario;
 		}
 		
 		public function getSalario() {
-			return $this->salario;
+			return $this->salario_fixo;
 		}
 	}
 ?>
