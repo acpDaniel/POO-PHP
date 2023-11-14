26a31c24620c4532c34ec18a43bc2e1ce5baea05
na1ara <naiarabarceloss@gmail.com>
2023-11-14 16:09:43 -0300

    criacao classe sistema

---
+++ code/class/class.sistema.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "class.gerencialogin.php"
+
+class Sistema{
+    //atributos protegidos
+    protected float $imposto_da_clinica
+    protected gerencialogin $gerenciaLogin
+
+    protected function calculaResultadoMensal(){
+        
+    }
+}
+
+?>
