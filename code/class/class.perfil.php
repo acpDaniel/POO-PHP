b58fdc948708852cd0173333e50ac726e06dda03
na1ara <naiarabarceloss@gmail.com>
2023-11-14 16:08:07 -0300

    adicionaFuncionalidades

--- code/class/class.perfil.php
+++ code/class/class.perfil.php
@@ -6,6 +6,11 @@ class Perfil {
     protected function getFuncionalidades(){
         return $funcionalidade_permitidas;
     }
+
+    private function adicionaFuncionalidades($nova_funcionalidade){
+        $this->funcionalidade_permitidas[] = $nova_funcionalidade;
+        return $funcionalidade_permitidas;
+    }
     
 }
 ?>
