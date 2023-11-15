512aa29dfbd74c48282ac54efc4cb2d3c10fa21a
Daniel <danielprojetos777@gmail.com>
2023-11-15 12:55:09 -0300

    criacao classe endereco

---
+++ code/class/class.endereco.php
@@ -0,0 +1,95 @@
+<?php
+
+class Endereco
+{
+    private $rua;
+    private $bairro;
+    private $numero;
+    private $cep;
+    private $cidade;
+    private $estado;
+    private $complemento;
+
+    public function __construct($rua, $bairro, $numero, $cep, $cidade, $estado, $complemento)
+    {
+        $this->rua = $rua;
+        $this->bairro = $bairro;
+        $this->numero = $numero;
+        $this->cep = $cep;
+        $this->cidade = $cidade;
+        $this->estado = $estado;
+        $this->complemento = $complemento;
+    }
+
+    // Métodos getters
+    public function getRua()
+    {
+        return $this->rua;
+    }
+
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    // Métodos setters
+    public function setRua($rua)
+    {
+        $this->rua = $rua;
+    }
+
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+    }
+
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    }
+
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+    }
+
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+    }
+
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
+
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+    }
+}
