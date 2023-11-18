8a47dee772a551069066ff0f878640fc02ed2911
B-Nic <145769988+B-Nic@users.noreply.github.com>
2023-11-18 15:59:55 -0300

    Update class.gerencialogin.php

--- code/class/class.gerencialogin.php
+++ code/class/class.gerencialogin.php
@@ -1,34 +1,42 @@
 <?php
 require_once('class.profissional.php');
-require_once('class.funcionalidades.php');
 class GerenciaLogin
 {
     
     static private GerenciaLogin $usuario_logado = null;
     private Funcionalidades $funcionalidades;
-    private function __construct()
-    {
-        
+    private string $usuario;
+    private string $senha;
+    private function __construct($usuario, $senha){
     }
+        $this -> usuario = $usuario;
+        $this -> senha = $senha
     
-    static function ValidaLogin():bool
+    private function ValidaLogin($usuario, $senha):bool
     {
-        return 0;
+        if(true){ #Verificar se o login e senha passados por parâmetro estão no persist
+            print("Login efetuado com sucesso.");
+            return true
+        }
+        print("Usuário e/ou senha inválidos.")
+        return false
+      
     }
     static function Logout()
     {
         if(self::$usuario_logado != null){
             print("Logout efetuado.");
-            $usuario_logado = null;
+            $self::usuario_logado = null;
         }
         else{
             print("Nenhum usuário em uso, impossível deslogar.");
         }
     }
-    static function ControlaLogins(){
+    static function ControlaLogins($usuario, $senha){
         if(self::$usuario_logado == null){
-            self::$usuario_logado == new GerenciaLogin();
-            print("Login efetuado com sucesso");
+            if(ValidaLogin($usuario, $senha)==true){
+                self::$usuario_logado == new GerenciaLogin();
+            }
         }
         else{
             print("Desconecte da sessão atual para poder logar.");
