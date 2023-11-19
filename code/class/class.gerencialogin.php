01591983a7c2b9d9d62c1155dddef3c762c7f0d4
B-Nic <145769988+B-Nic@users.noreply.github.com>
2023-11-19 00:13:36 -0300

    Update class.gerencialogin.php

--- code/class/class.gerencialogin.php
+++ code/class/class.gerencialogin.php
@@ -1,48 +1,49 @@
 <?php
-require_once('class.profissional.php');
+
 class GerenciaLogin
 {
     
-    static private GerenciaLogin $usuario_logado = null;
+    static private ?GerenciaLogin $usuario_logado = null;
     private Funcionalidades $funcionalidades;
     private string $usuario;
     private string $senha;
-    private function __construct(){
+    
+    private function __construct($usuario, $senha){
+        if($this->ValidaLogin($usuario, $senha) == true){
+            $this -> usuario = $usuario;
+            $this -> senha = $senha;
+        }
+        else{
+            self::$usuario_logado = null;
+        }
     }
-       
     private function ValidaLogin($usuario, $senha):bool
     {
         if(true){ #Verificar se o login e senha passados por parâmetro estão no persist
             print("Login efetuado com sucesso.");
-            return true
-        };
+            return true;
+        }
         print("Usuário e/ou senha inválidos.");
         return false;
       
     }
-
     static function Logout()
     {
         if(self::$usuario_logado != null){
             print("Logout efetuado.");
-            $self::usuario_logado = null;
+            self::$usuario_logado = null;
         }
         else{
             print("Nenhum usuário em uso, impossível deslogar.");
         }
     }
-
-
     static function ControlaLogins($usuario, $senha){
         if(self::$usuario_logado == null){
-            if(ValidaLogin($usuario, $senha)==true){
-                self::$usuario_logado == new GerenciaLogin($usuario, $senha);
-            }
+        self::$usuario_logado = new GerenciaLogin($usuario, $senha);
         }
-        else{
+        else{ 
             print("Desconecte da sessão atual para poder logar.");
         }
         return self::$usuario_logado;
     } 
 }
-?>
